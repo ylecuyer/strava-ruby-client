6e558bdfa7cda09a6d7d984444a199c6b1c3fe56
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2019-12-27 16:08:49 +0100

    New Strava::Errors::UploadFailed error for /uploads endpoint

--- lib/strava/web/raise_error.rb
+++ lib/strava/web/raise_error.rb
@@ -5,6 +5,8 @@ module Strava
         ClientErrorStatuses = (400...600).freeze
 
         def on_complete(env)
+          handle_upload_errors(env) if env.url.to_s.end_with?('/uploads')
+
           case env[:status]
           when 404
             raise Faraday::Error::ResourceNotFound, response_values(env)
@@ -14,6 +16,10 @@ module Strava
           when ClientErrorStatuses
             raise Strava::Errors::Fault, response_values(env)
           end
+        end
+
+        def handle_upload_errors(env)
+          raise Strava::Errors::UploadFailed, response_values(env) if env.status != 200 && env.body.has_key?('error') && !env.body['error'].blank?
         end
 
         def response_values(env)
