73fca06d43a008d178b4ca75151d0cec4b6fcafc
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2020-01-12 18:42:14 +0100

    better condition with uri

--- lib/strava/web/raise_error.rb
+++ lib/strava/web/raise_error.rb
@@ -5,7 +5,7 @@ module Strava
         ClientErrorStatuses = (400...600).freeze
 
         def on_complete(env)
-          handle_upload_errors(env) if env.url.to_s.end_with?('/uploads')
+          handle_upload_errors(env) if env.method = :post && env.url.path.end_with?('/uploads')
 
           case env[:status]
           when 404
